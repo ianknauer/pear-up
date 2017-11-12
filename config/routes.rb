c35de467e66841e96b87f1210ace9b6940b61c6d
ian knauer <ian.knauer@gmail.com>
2017-11-12 00:56:00 -0800

    fixes for devise-auth through API

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
 
   resources :users
+  devise_for :users, controllers: { sessions: 'sessions' }
 
   namespace :api do
     namespace :v1 do
-      devise_for :users, controllers: { sessions: 'sessions' }
-      # jsonapi_resources :users do
-      #   jsonapi_resources :interests
-      # end
+      jsonapi_resources :users do
+        jsonapi_resources :interests
+      end
     end
   end
 end
